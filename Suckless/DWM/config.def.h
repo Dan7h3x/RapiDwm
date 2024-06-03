5457e3cae301e3ca574226b60545a82bc93810dd
Dan7h3x <m.jalili.barbin@gmail.com>
2024-06-03 18:50:16 +0330

    Added stations

--- Suckless/DWM/config.def.h
+++ Suckless/DWM/config.def.h
@@ -186,11 +186,11 @@ static void (*bartabmonfns[])(Monitor *) = {NULL /* , customlayoutfn */};
 #endif // MONOCLE_LAYOUT
 #endif // BAR_TABGROUPS_PATCH
 #if BAR_PANGO_PATCH
-static const char font[] = {"JetBrainsMono Nerd Font:size=12"};
+static const char font[] = {"CaskaydiaCove Nerd Font:size=14"};
 #else
-static const char *fonts[] = {"JetBrainsMono Nerd Font:size=12"};
+static const char *fonts[] = {"CaskaydiaCove Nerd Font:size=14"};
 #endif // BAR_PANGO_PATCH
-static const char dmenufont[] = "FantasqueSansMono Nerd Font:size=14";
+static const char dmenufont[] = "CaskaydiaCove Nerd Font:size=14";
 #include "themes/tokyo.h"
 // static char urgfloatcolor[]              = "#db8fd9";
 
